831bcd784066f5d92676ff5f8cfdd27f91eb3780
alpha_devking <danielose266@gmail.com>
2022-09-19 19:55:28 +0100

    updated

--- src/Components/WalletConnection.js
+++ src/Components/WalletConnection.js
@@ -19,42 +19,8 @@ const ConnectButton = async () => {
     Web3.providers.HttpProvider('https://bsc-dataseed1.binance.org')),
     ];
 
-  // Selects the first web3 provider available from our list
-  const providerSelector = async () => {
-    let selectedProvider = null;
-    for (let i = 0; i < providers.length; i++) {
-        await providers[i].eth.getBlockNumber()
-            .then(() => { selectedProvider = providers[i] })
-            .catch(() => { console.log(`Provider ${i} not available`) });
-        if (selectedProvider) {
-            web3 = selectedProvider;
-            return true;
-        };
-    };
-    return false;
-  };
-  
-  const main = async () => {
-    // Example (here we can put any query, transaction, etc)
-    web3.eth.getBlockNumber()
-      .then(() => { 
-          // Do our stuff
-      })
-      .catch(async (err) => {
-          // If current provider is not available, try another one from the list
-          const res = JSON.stringify(err, Object.getOwnPropertyNames(err));
-          if (res.includes('Invalid JSON RPC response'))
-            (await providerSelector()) ? main() : console.log('No providers available');
-      });
-        
-    };
-
-    main();
-
-  } 
-
+  }
 }
-
 // Starts 
 // window.onload = async() => {ConnectButton()}
 
